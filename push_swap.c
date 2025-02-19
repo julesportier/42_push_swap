f58426a5dfdc81e082fc481d2890311ca54c2c51
Jules Portier <juportie@z2r9p1.42lyon.fr>
2025-02-19 14:32:10 +0100

    feat: basic cdlist functions in libft (new, addfront, print)

---
+++ push_swap.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juportie <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/19 11:42:27 by juportie          #+#    #+#             */
+/*   Updated: 2025/02/19 13:58:11 by juportie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+#include "libft/src/libft.h"
+#include <stdlib.h>
+
+int	main(void)
+{
+	t_dlisti	*lst = NULL;
+
+	t_dlisti	*new_node = ft_dlsti_new(1);
+	ft_cdlsti_add_back(&lst, new_node);
+	ft_print_dlsti(lst);
+	t_dlisti	*new_node2 = ft_dlsti_new(2);
+	ft_cdlsti_add_back(&lst, new_node2);
+	ft_print_dlsti(lst);
+	t_dlisti	*new_node3 = ft_dlsti_new(3);
+	ft_cdlsti_add_back(&lst, new_node3);
+	ft_print_dlsti(lst);
+	return (0);
+}
